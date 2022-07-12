e8de8c52690f6c7a2d6c3a0b020b03e426d67fae
Ulad_Savva <uladzislau.savva@gmail.com>
2022-07-13 01:54:14 +0300

    Initial commit

---
+++ LinkShortening/settings/develop.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'link-shortening',
+        'USER': 'postgres',
+        'PASSWORD': 'QT8VkpX#',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
