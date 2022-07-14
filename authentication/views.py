179fa6e2d3a70cc5ea9ddf60c6755d362e960327
Ulad_Savva <uladzislau.savva@gmail.com>
2022-07-14 23:12:27 +0300

    Profile page implemented Small styling fixes

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import TemplateView, CreateView
+from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
+
+from general.models import ShortenedLink
 
 
 class LoginView(auth_views.LoginView):
@@ -16,5 +19,16 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
 
 
-class ProfileView(TemplateView):
+class ProfileView(ListView):
     template_name = 'authentication/profile.html'
+    paginate_by = 15
+    model = ShortenedLink
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        links = ShortenedLink.objects.order_by('pk').filter(user=self.request.user)
+        paginator = Paginator(links, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+        return context
