6ba616640e02d84102f018293dcb610832947197
Ulad_Savva <uladzislau.savva@gmail.com>
2022-07-14 18:29:47 +0300

    Added model for short URLs

--- general/models.py
+++ general/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShortenedLink(models.Model):
+    long_url = models.URLField(unique=True)
+    short_key = models.CharField(max_length=10, unique=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='shortened_links', null=True)
+
+    class Meta:
+        verbose_name = 'Сокращённая ссылка'
+        verbose_name_plural = 'Сокращённые ссылки'
